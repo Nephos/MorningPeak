b0c93a55f2698ebde0fc121b3e97cee1df4e0a20
Arthur Poulet <arthur.poulet@cryptolab.eu>
2015-06-17 02:43:10 +0200

    improve client-contact relations

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,12 @@ class Client < ActiveRecord::Base
   has_many :bills
   belongs_to :contact
 
+  delegate :name, to: :contact, allow_nil: true, prefix: true
+
+  def last_contact
+    contacts.pluck('last_contact').max || "never"
+  end
+
   def bills_retard
     bills.where("due_date <= '#{Date.today}'").where(paid: false)
   end
