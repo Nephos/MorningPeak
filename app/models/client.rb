7ccae2223833cee9002221ec6101e972f42dfdf2
Arthur Poulet <arthur.poulet@cryptolab.eu>
2015-06-17 02:30:58 +0200

    Add bootstrap, fix every views, fix routes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,22 @@
 class Client < ActiveRecord::Base
   has_many :contacts
   has_many :bills
+  belongs_to :contact
+
+  def bills_retard
+    bills.where("due_date <= '#{Date.today}'").where(paid: false)
+  end
+
+  def bills_advanced
+    bills.where("due_date > '#{Date.today}'").where(paid: true)
+  end
+
+  def bills_next
+    bills.where("due_date > '#{Date.today}'").where(paid: false)
+  end
+
+  def bills_old
+    bills.where("due_date <= '#{Date.today}'").where(paid: true)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "clients#index"
   resources :contacts
   resources :bills
-  resources :clients
+  resources :clients do
+    resources :contacts
+    resources :bills
+  end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
