6b482929eeffef82a811330159757f0e90e3972a
Arthur Poulet <arthur.poulet@cryptolab.eu>
2015-06-19 21:35:48 +0200

    move ticket helper

--- app/helpers/ticket_clients_helper.rb
+++ app/helpers/ticket_clients_helper.rb
@@ -1,2 +1,8 @@
 module TicketClientsHelper
+
+  def user_tickets_unview_count
+    return nil if not user_signed_in?
+    return current_user.tickets_unview.count
+  end
+
 end
--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -8,11 +8,6 @@ module TicketsHelper
     end
   end
 
-  def user_tickets_unview_count
-    return nil if not user_signed_in?
-    return current_user.tickets_unview.count
-  end
-
   def admin_tickets_unview_count
     return if not admin_signed_in?
     return current_admin.tickets_unview.count
